61c65ef32a9e7bc1eb563db9582fa540c23fa9db
Mokshit Tarunkumar Shah <mokshit.shah05@gmail.com>
2026-07-27 13:05:45 +0400

    SUPABASE API ANON KEY AND URL

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // This file holds your Supabase connection details.
 // Get these from your Supabase project: Settings > API
 
-const SUPABASE_URL = "PASTE_YOUR_SUPABASE_URL_HERE";
-const SUPABASE_ANON_KEY = "PASTE_YOUR_ANON_KEY_HERE";
+const SUPABASE_URL = "https://jcsgmnkibyhadqbqknlq.supabase.co/rest/v1/";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impjc2dtbmtpYnloYWRxYnFrbmxxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxMzM2ODEsImV4cCI6MjEwMDcwOTY4MX0.nNNFZjjJgVz5Z75Q4ZbkrE8HMX2DQI51R0xWUe6h16Q";
